3456c64c65599599f5efcb8167c522de0c51477d
sundaramguru <62461432+sundaramguru@users.noreply.github.com>
2022-07-09 18:35:54 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "bulma/css/bulma.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
